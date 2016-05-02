46325207de16a9906d8fede3de2a80e650ac43dd
jorgegabrich <jorgegabrich@users.noreply.github.com>
2016-05-01 22:21:58 -0300

    First Commit

---
+++ js/mouthwash.js
@@ -0,0 +1,7 @@
+$('.hover').mouseover(function() {
+  $('.portfolio-text').css("visibility","visible");
+});
+
+$('.hover').mouseout(function() {
+  $('.portfolio-text').css("visibility","hidden");
+});
